5a79ba3084d3733787cbac44064427b778b58178
Atary777 <joaopaulodossantosaraujo51@gmail.com>
2026-04-28 22:29:12 -0300

    preencher_script

---
+++ script.js
@@ -0,0 +1,16 @@
+// let largura = document.getElementById('largMM');
+// let altura = document.getElementById('altMM');
+
+function mostrarMensagem() {
+    const largura = parseFloat(document.getElementById('largMM').value) / 1000;
+    const altura = parseFloat(document.getElementById('altMM').value) / 1000;
+
+    const area = largura * altura;
+    const perimetro = ( largura + altura ) * 2 ;
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `
+    <p>Area da geometria: ${area.toFixed(2)} m². </p>
+    <p>Perímetro da geometria: ${perimetro.toFixed(2)} m. </p>
+    `
+}
