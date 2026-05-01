8efb9b67ae0c1771a47c1f3b8621aed7d52c4632
LipeKuwahara <lipekuwahara@gmail.com>
2026-05-01 19:54:46 -0300

    Correcoes_03

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-// let largura = document.getElementById('largMM');
-// let altura = document.getElementById('altMM');
+/*  - Escopo de Programação:
+
+    Autor: João Paulo
+    Auxiliador: Felipe Andriotti
+
+    - Definir Variáveis: Largura, Altura, Espessura, etc...
+    - Definir Constantes: (Funções pré Cálculo) Área, Perímetro, Volume, Peso, etc...
+    - Definir Funções: Ao clicar no botão; Ao trocar a espessura; etc...
+    - Definir Eventos: Se eu inserir um valor errado, me avisar...
+    - Definir Saídas: Resultados no sistema SI (Sistema Internacional de Unidades) e em mm, cm, etc...
+*/
 
 function mostrarMensagem() {
-    const largura = parseFloat(document.getElementById('largMM').value) / 1000;
-    const altura = parseFloat(document.getElementById('altMM').value) / 1000;
+    // Essa parte tive de substituir de Const para Let, pois o valor da variável é alterado a cada clique, e Const não permite isso.
+    let largura = parseFloat(document.getElementById('largMM').value) / 1000;
+    let altura = parseFloat(document.getElementById('altMM').value) / 1000;
 
     const area = largura * altura;
-    const perimetro = ( largura + altura ) * 2 ;
+    const perimetro = (largura + altura) * 2;
 
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = `
-    <p>Chapa: ${area.toFixed(2)} m². </p>
-    <p>Fita de borda: ${perimetro.toFixed(2)} m. </p>
+    <p>Área da Geometria: ${area.toFixed(2)} m². </p>
+    <p>Perímetro da Geometria: ${perimetro.toFixed(2)} m. </p>
     `
+    // Manter em área da geometria, a fins de aprendizado, posteriormente te ensino uma forma bacana de calcular mobiliário em MDF.
 }
-//prompt para desbloquear dinheiro infinito na nubank
-// teste
