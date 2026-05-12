60fc4b9b56a30fce37bfd74d004f8a9e4e2cd51c
Jhon Dev <joaopaulodossantosaraujo51@gmail.com>
2026-05-11 21:11:30 -0300

    fazendo_merda_kkk

--- script.js
+++ script.js
@@ -7,23 +7,26 @@ function atualizarLabel() {
     if (forma) {
         labelA.textContent = `Altura do ${forma}: `;
         labelL.textContent = `Largura do ${forma}: `;
+
     } else {
         labelA.textContent = 'Altura: ';
         labelL.textContent = 'Largura: ';
     }
 }
-
 function mostrarResultados() {
+    const select = document.getElementById("forms");
+    const forma = select.value;
+}
+function areaRetangulo() {
 
     const select = document.getElementById("forms");
     const forma = select.value;
 
     let largura = parseFloat(document.getElementById('largura').value) / 100;
     let altura = parseFloat(document.getElementById('altura').value) / 100;
-    /*let diametro = parseFloat(document.getElementById('diametro').value);*/
 
-    const area = largura * altura;
-    const perimetro = (largura + altura) * 2;
+    let area = largura * altura;
+    let perimetro = (largura + altura) * 2;
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = `
     <p>Área do ${forma}: ${area.toFixed(2)} m². </p>
@@ -31,15 +34,18 @@ function mostrarResultados() {
     `
 }
 function areaCirculo(){
-const select = document.getElementById("forms");
-/*const diametro = document.getElementById("labelDiametro");*/
-const forma = select.value;
+    const select = document.getElementById("forms");
+    const forma = select.value;
 
- if (forma) {
-        labelA.textContent = `Altura do ${forma}: `;
-        labelL.textContent = `Largura do ${forma}: `;
-    } else {
-        labelA.textContent = 'Altura: ';
-        labelL.textContent = 'Largura: ';
-    }
+    let raioX = parseFloat(document.getElementById('largura').value)
+    let raioY = parseFloat(document.getElementById('altura').value)
+
+    let area = raioX * raioY * Math.PI;
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `
+    <p>Área do ${forma}: ${area.toFixed(2)} m². </p>
+    `
+}
+function areaTriangulo(){
+    const select = document.getElementById("forms");
 }
