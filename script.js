754301bf54c87a50d53375fff4fee9a2e77e6262
Jhon Dev <joaopaulodossantosaraujo51@gmail.com>
2026-05-15 14:56:10 -0300

    pequeno ajuste na const

--- script.js
+++ script.js
@@ -24,13 +24,26 @@ const constMAP = {
     tipoTriangulo: document.getElementById("triangulos"),
     largura: document.getElementById("largura"),
     altura: document.getElementById("altura"),
-    labelLargura: document.getElementById("labelLargura"),
-    labelAltura: document.getElementById("labelAltura"),
-    resultado: document.getElementById("resultado"),
     raioX: document.getElementById("raioX"),
     raioY: document.getElementById("raioY"),
+    angulo1: document.getElementById("angulo_1"),
+    angulo2: document.getElementById("angulo_2"),
+    angulo3: document.getElementById("angulo_3"),
+    baseInferior: document.getElementById("baseinf"),
+    baseSuperior: document.getElementById("basesup"),
+    alturaTrapezio: document.getElementById("alt"),
+
+    labelLargura: document.getElementById("labelLargura"),
+    labelAltura: document.getElementById("labelAltura"),
     labelRX: document.getElementById("labelRX"),
     labelRY: document.getElementById("labelRY"),
+    labelinf: document.getElementById("labelinf"),
+    labelsup: document.getElementById("labelsup"),
+    labelalt: document.getElementById("labelalt"),
+    labelang1: document.getElementById("labelang1"),
+    labelang2: document.getElementById("labelang2"),
+    labelang3: document.getElementById("labelang3"),
+    resultado: document.getElementById("resultado"),
 };
 
 function atualizarLabel() {
@@ -50,8 +63,8 @@ function atualizarLabel() {
 }
 
 function areaRetangulo() {
-    //const select = document.getElementById("forms");
-    //const forma = select.value;
+    const select = document.getElementById("forms");
+    const forma = select.value;
 
     const forma = constMAP.select.value;
 
@@ -63,38 +76,27 @@ function areaRetangulo() {
 
 }
 
-function areaCirculo() {
-    //const raioX = parseFloat(document.getElementById("raioX").value)
-    //const raioY = parseFloat(document.getElementById("raioY").value)
-    //EU SEI QUE TA ERRADO//
-}
+//function areaCirculo() {
+//}
 
-function areaTriangulo() {
-    //const tipo = constMAP.tipoTriangulo;
-    //const select = constMAP.select;
-    //EU SEI QUE TA ERRADO//
-}
+//function areaTriangulo() {
+//}
 
-function areaTrapezio() {
-    //const tipo = constMAP.tipoTriangulo;
-    //const select = constMAP.select;
-    //EU SEI QUE TA ERRADO//
-}
+//function areaTrapezio() {
+//}
 
-function areaQuadrado() {
+//function areaQuadrado() {
 
-    //const tipo = constMAP.tipoTriangulo;
-    //const select = constMAP.select;
-    //EU SEI QUE TA ERRADO//
-}
+//}
 
 function mostrarResultados() {
-    const select = document.getElementById("forms");
+    const select = constMAP.select.value
     const forma = select.value;
-
+  
     let area = constMAP.largura.value / 1000 * constMAP.altura.value / 1000;
     let perimetro = (constMAP.largura.value / 1000 + constMAP.altura.value / 1000) * 2;
     d
+
     const resultado = constMAP.resultado;
     resultado.innerHTML = `
     <p>Área do ${forma}: ${area.toFixed(2)} m². </p>
